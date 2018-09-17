3f7aa652cdfbf0d155a7be6a2d2a52a0eb2c5279
jooyeaon <41285078+jooyeaon@users.noreply.github.com>
2018-09-17 15:04:38 +0900

    cosemticAjaxController

---
+++ Hwahea/src/main/java/kr/spring/cosmetic/controller/CosmeticAjaxController.java
@@ -0,0 +1,46 @@
+package kr.spring.cosmetic.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.spring.cosmetic.domain.CosmeticCommand;
+import kr.spring.cosmetic.service.CosmeticService;
+
+@Controller
+public class CosmeticAjaxController {
+	private Logger log = Logger.getLogger(this.getClass());	
+	
+	@Resource
+	private CosmeticService cosmeticService;
+	
+	//브랜드별 랭킹 호출
+	@RequestMapping("/ranking/brand_ranking.do")
+	@ResponseBody
+	public Map<String, Object> getBrandRanking(@RequestParam("brand_num") String brand_num){
+		
+		if(log.isDebugEnabled()) {
+			log.debug("<<brand_num>> : "+brand_num);
+		}
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("brand_num", brand_num);
+		map.put("c_code", "B"+brand_num+"%");
+		
+		List<CosmeticCommand> list = null;
+		list = cosmeticService.getCosmeticRank(map);
+		
+		map.put("list", list);
+		
+		return map;
+	}
+	
+}
